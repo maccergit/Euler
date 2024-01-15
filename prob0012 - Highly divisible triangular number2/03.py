33bbf8ba409b55f1c7503f071139011e53bdbe4f
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-01-15 13:10:55 -0700

    Fix name

---
+++ prob0012 - Highly divisible triangular number2/03.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 19, 2020
+
+@author: johnmcalister
+'''
+
+# pyprimesieve
+
+from pyprimesieve import factorize
+import math
+
+def triangle(n):
+    return int((n + 1) * n / 2)
+
+assert triangle(1) == 1
+assert triangle(2) == 3
+assert triangle(3) == 6
+assert triangle(4) == 10
+assert triangle(5) == 15
+assert triangle(6) == 21
+assert triangle(7) == 28
+assert triangle(8) == 36
+assert triangle(9) == 45
+assert triangle(10) == 55
+
+def num_divisors(n):
+    return math.prod(x[1] + 1 for x in factorize(n))
+
+def solution(limit):
+    current = 1
+    while num_divisors(triangle(current)) <= limit:
+        current += 1
+    return triangle(current)
+
+assert solution(5) == 28
+print(solution(500))
+
+count = 100
+scale = 1000 # msec
+
+import utils.timing
+utils.timing.table_timing([10, 500], count, scale)
+utils.timing.plot_timing([100, 200, 300, 400, 500], count, scale)
---
+++ prob0012 - Highly divisible triangular number2/05.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 19, 2020
+
+@author: johnmcalister
+'''
+
+# SymPy with coprimes
+
+from sympy import divisor_count
+
+def triangle(n):
+    return int((n + 1) * n / 2)
+
+assert triangle(1) == 1
+assert triangle(2) == 3
+assert triangle(3) == 6
+assert triangle(4) == 10
+assert triangle(5) == 15
+assert triangle(6) == 21
+assert triangle(7) == 28
+assert triangle(8) == 36
+assert triangle(9) == 45
+assert triangle(10) == 55
+
+def triangle_divisors(n):
+    if n % 2 == 0:
+        return divisor_count(n // 2) * divisor_count(n + 1)
+    return divisor_count(n) * divisor_count((n + 1) // 2)
+
+def solution(limit):
+    current = 1
+    while triangle_divisors(current) <= limit:
+        current += 1
+    return triangle(current)
+
+assert solution(5) == 28
+print(solution(500))
+
+count = 10
+scale = 1000 # msec
+
+import utils.timing
+utils.timing.table_timing([10, 500], count, scale)
+utils.timing.plot_timing([100, 200, 300, 400, 500], count, scale)
