6f69d992c1caabdff7a9252070850be5e60889c6
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-01-23 23:34:09 -0700

    Initial commit for prob0016

---
+++ prob0016 - Power Digit Sum/01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Jan 23, 2024
+
+@author: johnmcalister
+
+Direct approach
+'''
+
+
+def solution(limit):
+    return sum(int(x) for x in str(2 ** limit))
+
+assert solution(15) == 26
+print(solution(1000))
+
+count = 10000
+scale = 1000000
+
+import utils.timing
+utils.timing.table_timing([15, 1000], count, scale)
+utils.timing.plot_timing([100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500], count, scale, "prob0016.01", ticks=False)
