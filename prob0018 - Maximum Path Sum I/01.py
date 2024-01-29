a0910deb185a2924adf6204831c6c2c33867221c
johnmcalister <johnmcalister@192.168.1.186>
2024-01-29 01:14:47 -0700

    Prob0018 - initial commit of problem description and code stub

---
+++ prob0018 - Maximum Path Sum I/01.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Jan 29, 2024
+
+@author: johnmcalister
+
+Direct approach - WORK IN PROGRESS
+'''
+
+sample_data = []
+problem_data = []
+
+def solution(data):
+    return 0
+
+assert solution(sample_data) == 23
+print(solution(problem_data))
+
+count = 1
+scale = 1
+
+# import utils.timing
+# utils.timing.table_timing([5, 1000], count, scale)
+# utils.timing.plot_timing([100, 200, 300, 400, 500, 600, 700, 800, 900, 1000], count, scale, "prob0018.01")
