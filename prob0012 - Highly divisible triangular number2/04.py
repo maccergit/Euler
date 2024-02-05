1fe90b7acb33c8c9db80ec8f88af29772e3d4228
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-02-05 12:11:43 -0700

    Remove obsolete tests left over from older version

--- prob0012 - Highly divisible triangular number2/04.py
+++ prob0012 - Highly divisible triangular number2/04.py
@@ -27,12 +27,8 @@ assert triangle(9) == 45
 assert triangle(10) == 55
 
 def num_divisors(n):
-    factors = {}
     x = [y for y in utils.factor.gen_factors(n)]
-    for y in {z for z in x}:
-        factor_count = x.count(y)
-        if y not in factors or factor_count > factors[y]:
-            factors[y] = factor_count
+    factors = {y : x.count(y) for y in {z for z in x}}
     return math.prod(factors[factor] + 1 for factor in factors)
 
 def triangle_divisors(n):
