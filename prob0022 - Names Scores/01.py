2b69883dc32b3ea0cf418c0c08a33d3bf5b5e08a
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-02-06 11:10:04 -0700

    Initial commit of stub for prob0022

---
+++ prob0022 - Names Scores/01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 6, 2024
+
+@author: johnmcalister
+
+Direct approach
+'''
+
+def solution(limit):
+    return 0
+
+print(solution(0))
+
+count = 1
+scale = 1
+
+# import utils.timing
+# utils.timing.table_timing([1000, 10000], count, scale)
+# utils.timing.plot_timing([1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000], count, scale, "prob0022.01")
