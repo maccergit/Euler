b0b1321137d4d2585ac2d4e82ec929edbccf5857
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-02-10 12:56:15 -0700

    Initial commit of stub for prob0025

---
+++ prob0025 - 1000-digit Fibonacci Number/01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 6, 2024
+
+@author: johnmcalister
+
+Direct approach
+'''
+
+def solution(limit):
+    return 0
+
+assert solution(2) == 7
+assert solution(3) == 12
+
+print(solution(1000))
+
+count = 1
+scale = 1
+
+# import utils.timing
+# utils.timing.table_timing([1000000], count, scale)
+# utils.timing.plot_timing([100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000], count, scale)
