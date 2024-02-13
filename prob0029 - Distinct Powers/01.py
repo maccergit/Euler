e4e59cad3571f257dbc2e610b1742a21f99c7d97
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-02-13 11:53:06 -0700

    initial stub for prob0029

---
+++ prob0029 - Distinct Powers/01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 13, 2024
+
+@author: johnmcalister
+
+Direct approach
+'''
+
+def solution(limit):
+    return 0
+
+assert solution(5) == 15
+
+print(solution(100))
+
+count = 1
+scale = 1
+
+# import utils.timing
+# utils.timing.table_timing([5, 100], count, scale)
+# utils.timing.plot_timing([101, 201, 301, 401, 501, 601, 701, 801, 901, 1001], count, scale)
