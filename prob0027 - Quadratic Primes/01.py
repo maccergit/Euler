31d3a8e640fe5b24fe3b95f2389080b87db18540
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2024-02-12 23:15:15 -0700

    Initial solutions for prob0027 - investigate if there are optimizations.

---
+++ prob0027 - Quadratic Primes/01.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 12, 2024
+
+@author: johnmcalister
+
+Direct approach - iterate through a and b, finding number of primes each generates.  Primality test is factoring to see if any factors > 1 exist.
+'''
+
+import utils.factor as factor
+
+def numPrimes(a, b):
+    n = 0
+    p = 1
+    factors = [0]
+    while len(factors) == 1:
+        n += 1
+        p = n * n + a * n + b
+        factors = [x for x in factor.gen_factors(p)]
+    return n
+
+# assert numPrimes(1, 41) == 40
+# assert numPrimes(-79, 1601) == 80
+
+def solution(limit):
+    maxResult = -1
+    for a in range(1-limit, limit):
+        for b in range(-limit, limit + 1):
+            result = numPrimes(a, b)
+            if result > maxResult:
+                maxResult = result
+                maxAB = (a, b)
+    return maxAB[0] * maxAB[1]
+
+assert solution(100) == -1455
+# print(solution(1000))
+
+count = 10
+scale = 1000
+
+import utils.timing
+utils.timing.table_timing([40, 100], count, scale)
+utils.timing.plot_timing([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], count, scale)
---
+++ prob0027 - Quadratic Primes/02.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 12, 2024
+
+@author: johnmcalister
+
+Direct approach - iterate through a and b, finding number of primes each generates.  Primality test is factoring to see if any factors > 1 exist.
+'''
+
+import utils.fastfactor as factor
+
+def numPrimes(a, b):
+    n = 0
+    p = 1
+    factors = [0]
+    while len(factors) == 1:
+        n += 1
+        p = n * n + a * n + b
+        factors = [x for x in factor.gen_factors(p)]
+    return n
+
+# assert numPrimes(1, 41) == 40
+# assert numPrimes(-79, 1601) == 80
+
+def solution(limit):
+    maxResult = -1
+    for a in range(1-limit, limit):
+        for b in range(-limit, limit + 1):
+            result = numPrimes(a, b)
+            if result > maxResult:
+                maxResult = result
+                maxAB = (a, b)
+    return maxAB[0] * maxAB[1]
+
+print(solution(1000))
+
+count = 1
+scale = 1
+
+import utils.timing
+utils.timing.table_timing([40, 1000], count, scale)
+utils.timing.plot_timing([100, 200, 300, 400, 500], count, scale)
