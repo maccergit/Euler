c172207d7b89f3494e8a817a64a8b0d81e59a935
johnmcalister <johnmcalister@c02f615hmd6w.lan>
2025-02-16 18:12:38 -0700

    Add closed form solution developed with AI help

---
+++ prob0002 - even Fibonacci numbers/06.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+Created on Feb 19, 2020
+
+@author: johnmcalister
+
+Use closed form of sum of even fibonacci numbers.
+'''
+
+import math
+
+def solution(limit):
+    root5 = (5 ** 0.5)
+    phi = (1 + root5) / 2
+    upsilon = (1 - root5) / 2
+    exp = 3 * int((math.log(limit) + math.log(5) / 2.0) / math.log(phi) / 3) + 2
+    return int((phi ** exp - upsilon ** exp - root5) / (2 * root5))
+
+assert solution(100) == 44
+print(solution(4000000))
+
+count = 100000
+scale = 1000000
+
+import utils.timing
+utils.timing.table_timing([100, 4000000], count, scale)
+utils.timing.plot_timing([10000000, 20000000, 30000000, 40000000, 50000000, 60000000, 70000000, 80000000, 90000000, 100000000], count, scale)
